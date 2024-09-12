32665d1038720e968aece59f9459577d0cc85f44
ahmedzakaria2003 <ahmedmohamedzakaria423@gmail.com>
2024-09-12 16:31:54 +0300

    Add files via upload

---
+++ js/string.js
@@ -0,0 +1,39 @@
+//index  01234567
+// var str="web design and development and ui";
+
+// var result =str.replace('and','&')
+// var result =str.replaceAll('and','&')
+
+// var result =str.charAt(1).repeat(4)
+// console.log(result);
+
+// var result =str.split(' ').slice(0,2).join(' ')
+
+// slice(start,end(not included))
+// var result = str.slice(-4,-1)
+// var result = str.substring(1,3)
+
+// var result =str.concat('fun')
+
+// console.log(str.trim())
+// console.log(str.trimEnd())
+// console.log(str.trimStart())
+
+
+// var result = str.toLowerCase();
+// var result = str.toUpperCase();
+// console.log(result)
+
+// var result =str.charAt(0)
+// var result =str.at(-1)
+// var result =str[0]
+// console.log(result)
+
+
+
+// var result ='5'.padEnd(6,'0');
+// var result ='5'.padStart(6,'0');
+// console.log(result)
+
+
+// includes() boolean true - false
